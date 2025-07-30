6e27d89daae555770c459eb8f52bcee06bd794e1
Charles-Obi <charleswilliamsobiero@gmail.com>
2025-07-30 16:04:07 +0300

    Recents

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    status = models.BooleanField(default=False, verbose_name="Completed")
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}\n status{self.status}"
+
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
     return HttpResponse("This is our basic todo app")
 
 def index(request):
-    return render(request, 'todo_app/index.html')
+    context = {
+        'name': 'Charlie',
+    }
+    return render(request, 'todo_app/index.html', context) 
